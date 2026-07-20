4c13c1c8b4835d9b518dc6e08c08d459001b6da9
Arley Giraldo <arleygiraldo816@gmail.com>
2026-07-19 22:03:13 -0500

    Add files via upload

---
+++ Ensayo4ejercicio3/src/ensayo4ejercicio3/Ensayo4ejercicio3.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ensayo4ejercicio3;
+
+/**
+ *
+ * @author Arley Giraldo
+ */
+public class Ensayo4ejercicio3 {
+    public static void saludar(String nombre){
+        System.out.println("Bienvenido " + nombre + ".");
+    }
+    public static void main(String[] args) {
+     saludar("Carlos");
+    }
+    
+}
