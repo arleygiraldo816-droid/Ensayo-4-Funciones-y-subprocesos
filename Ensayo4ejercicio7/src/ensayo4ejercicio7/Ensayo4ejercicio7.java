09d58a32a4706c5e63556a5cdbe43829d029d38b
Arley Giraldo <arleygiraldo816@gmail.com>
2026-07-19 22:04:25 -0500

    Add files via upload

---
+++ Ensayo4ejercicio7/src/ensayo4ejercicio7/Ensayo4ejercicio7.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ensayo4ejercicio7;
+
+/**
+ *
+ * @author Arley Giraldo
+ */
+public class Ensayo4ejercicio7 {
+    public static double obtenerPi(){
+        return 3.1416;
+    }
+    public static void main(String[] args) {
+        double pi = obtenerPi();
+        System.out.println("El pi es: " +  pi );
+       
+    }
+    
+}
