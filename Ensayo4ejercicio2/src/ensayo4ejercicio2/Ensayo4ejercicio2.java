fc014e274f48c83eede5b388cd05194867fb63b5
Arley Giraldo <arleygiraldo816@gmail.com>
2026-07-19 23:15:25 -0500

    Add files via upload

---
+++ Ensayo4ejercicio2/src/ensayo4ejercicio2/Ensayo4ejercicio2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ensayo4ejercicio2;
+
+/**
+ *
+ * @author Arley Giraldo
+ */
+public class Ensayo4ejercicio2 {
+    public static void numerosPar(){
+    for (int i = 2; i <= 100; i = i + 2){
+        System.out.println(i);
+    }
+    }
+    public static void main(String[] args) {
+    numerosPar();
+    }
+    
+}
