19bf51ca6023882139f5525b6b3c9b8dc2027184
Arley Giraldo <arleygiraldo816@gmail.com>
2026-07-19 22:02:03 -0500

    Add files via upload

---
+++ Ensayo4ejercicio1/src/ensayo4ejercicio1/Ensayo4ejercicio1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ensayo4ejercicio1;
+
+/**
+ *
+ * @author Arley Giraldo
+ */
+public class Ensayo4ejercicio1 {
+    public static void bienvenida(){
+    System.out.println("Universidad: Utmach");
+    System.out.println("Materia: Tecnicas de programacion");
+    System.out.println("Nombre: Arley Josue Giraldo Borja");
+    }
+    
+     public static void main(String[] args) {
+        bienvenida();
+       } 
+}
